3722eec04f2e69d8417b8fc539966f2c92cb4ebc
wuyeelok <wuyeelok@gmail.com>
2019-01-01 21:03:07 +0800

    Third attempt

--- Kenneth_SQL/1.sql
+++ Kenneth_SQL/1.sql
@@ -2,7 +2,7 @@ CREATE TABLE songs (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     length FLOAT NOT NULL,
-    ablum_id INT,
+    album_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY(ablum_id) REFERENCES albums(id)
+    FOREIGN KEY (album_id) REFERENCES albums(id)
 );
