d76c4ab1c7a40cf6cf40d6e7d42ad8c7bcb51a1d
wuyeelok <wuyeelok@gmail.com>
2019-01-02 15:17:36 +0800

    Refactor code

--- Kenneth_SQL/5.sql
+++ Kenneth_SQL/5.sql
@@ -1,6 +1,5 @@
 SELECT b.name AS "Band Name"
 FROM bands AS b
-LEFT JOIN albums AS a
-ON b.id = a.band_id
+LEFT JOIN albums AS a ON b.id = a.band_id
 GROUP BY b.id 
-HAVING count(a.id) = 0;
+HAVING COUNT(a.id) = 0;
