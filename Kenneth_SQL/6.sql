58a4b869d361f38f6007fe80c4a1dbc2ffb063f2
wuyeelok <wuyeelok@gmail.com>
2019-01-07 15:58:29 +0800

    First attempt on ex-6

---
+++ Kenneth_SQL/6.sql
@@ -0,0 +1,9 @@
+SELECT a.name AS Name, 
+        a.release_year AS "Release Year",
+        SUM(a.length) AS Duration
+FROM albums AS a 
+INNER JOIN songs AS s 
+ON a.id = s.album_id
+GROUP BY a.id 
+ORDER BY Duration DESC
+LIMIT 1;
