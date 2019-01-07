2821e1769c1f2f9584c42becafcf951d02d91335
wuyeelok <wuyeelok@gmail.com>
2019-01-07 16:35:28 +0800

    Final attempt

--- Kenneth_SQL/7.sql
+++ Kenneth_SQL/7.sql
@@ -1,9 +1,3 @@
---Update the Release Year of the Album with no Release Year
-
 UPDATE albums AS t
 SET t.release_year = 1986
-WHERE t.id IN (
-    SELECT a.id 
-    FROM albums AS a 
-    WHERE a.release_year IS NULL
-);
+WHERE t.id = 4;
