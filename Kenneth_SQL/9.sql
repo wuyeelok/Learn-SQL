63591b3fbd193de70e2567d973704747fdffa8f4
wuyeelok <wuyeelok@gmail.com>
2019-01-09 11:19:45 +0800

    Second attempt for ex-9

--- Kenneth_SQL/9.sql
+++ Kenneth_SQL/9.sql
@@ -1,11 +1,19 @@
+SET SQL_SAFE_UPDATES = 0;
+
 DELETE FROM albums 
 WHERE id = (
-    SELECT id FROM albums 
-    WHERE name = 'First album' 
-    AND release_year = 2019 
-    AND band_id = (SELECT id FROM bands WHERE name = 'Rock Band' ORDER BY id DESC LIMIT 1)
-    ORDER BY id DESC LIMIT 1
+	SELECT d.id FROM
+    (SELECT a.id FROM albums AS a
+    WHERE a.name = 'First album' 
+    AND a.release_year = 2019 
+    AND a.band_id = (SELECT id FROM bands WHERE name = 'Rock Band' ORDER BY id DESC LIMIT 1)
+    ORDER BY a.id DESC LIMIT 1) AS d
     );
 
+
 DELETE FROM bands 
-WHERE id = (SELECT id FROM bands WHERE name = 'Rock Band' ORDER BY id DESC LIMIT 1);
+WHERE id = (
+    SELECT b.id FROM 
+        (
+            SELECT id FROM bands WHERE name = 'Rock Band' ORDER BY id DESC LIMIT 1
+        ) AS b);
