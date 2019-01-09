c980f3c6b3cbb83fadfb79a9acc8c7189bd291b7
wuyeelok <wuyeelok@gmail.com>
2019-01-09 17:27:02 +0800

    2nd Attempt for ex-12

--- Kenneth_SQL/12.sql
+++ Kenneth_SQL/12.sql
@@ -1,7 +1,8 @@
 SELECT b.name AS "Band",
 count(s.id) AS "Number of Songs"
 FROM bands AS b
-INNER JOIN album AS a 
+INNER JOIN albums AS a 
 ON b.id = a.band_id
 INNER JOIN songs AS s 
-ON a.id = s.album_id;
+ON a.id = s.album_id
+GROUP BY b.name;
