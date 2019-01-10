6c3694f5f533ae5e71f9ab87774f29fc8b09b75e
wuyeelok <wuyeelok@gmail.com>
2019-01-10 10:07:37 +0800

    1st Retry on ex-7

--- Kenneth_SQL/7.sql
+++ Kenneth_SQL/7.sql
@@ -1,3 +1,8 @@
+SET SQL_SAFE_UPDATES = 0;
+
 UPDATE albums AS t
 SET t.release_year = 1986
-WHERE t.id = 4;
+WHERE t.id = (SELECT a.id FROM albums AS a WHERE a.release_year IS NULL);
+
+
+SET SQL_SAFE_UPDATES = 1;
